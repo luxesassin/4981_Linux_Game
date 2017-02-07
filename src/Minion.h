376d14824841fcff1a82eddcac4b30675c2f5f72
Robert Arendac <robertarendac@gmail.com>
2017-02-06 17:03:59 -0800

    created basic move behaviour for zombie and modified A* to work more with a map

--- src/Minion.h
+++ src/Minion.h
@@ -18,33 +18,69 @@
 #ifndef MINION_H
 #define MINION_H
 
-#include "Point.h"
+#include "Astar.h"
 
 class Minion {
 public:
-    explicit Minion(const Point loc, const int hp = 100,
+    explicit Minion(const int xPos = 0, const int yPos = 0, const int hp = 100,
                     const int pwr = 100, const int amr = 50,
                     const int agil = 50, const int mode = 0)
-    : loc_(loc), hp_(hp), pwr_(pwr), amr_(amr),
+    : xPos_(xPos), yPos_(yPos), hp_(hp), pwr_(pwr), amr_(amr),
       agil_(agil), mode_(mode) {}
     virtual ~Minion() {}
 
     // methods
-    virtual void move() {}
-    virtual void attack() {}
+    virtual void move(int destX, int destY) {
+        std::vector<std::pair<int, int>> path;
 
-    virtual void hit()
-    {
-        if (--hp <= 0)
-        {
+        path = buildCoordinatePath(xPos_, yPos_, destX, destY);
+
+        setMode(1);
+
+        for (auto i = path.begin(); i != path.end(); ++i) {
+            setPosition(*i);
+            //Might want to wait or something so zombie doesn't teleport
+        }
+    }
+    virtual void attack() {
+        setMode(2);
+    }
+    virtual void die() {}
+
+    virtual void hit() {
+        hp_--;
+        if (hp_ <= 0) {
             die();
         }
     }
 
-    virtual void die() {}
+    virtual void setPosition(int xCoord, int yCoord) {
+        xPos_ = xCoord;
+        yPos_ = yCoord;
+    }
+
+    virtual void setPosition(std::pair<int, int> loc) {
+        xPos_ = loc.first;
+        yPos_ = loc.second;
+    }
+
+    virtual std::pair<int, int> getPostion() {
+        return std::make_pair(xPos_, yPos_);
+    }
+
+    virtual void setMode(int mode) {
+        if (mode < 3 && mode >= 0) {
+            mode_ = mode;
+        }
+    }
 
 private:
-    Point loc_;     // minion location
+    /* Some of these variables may not be used for the final game.  These
+    were made with the thought of having multiple zombie types, so it's fine
+    if they are deleted.  They don't really do too much right now. */
+
+    int xPos_;      // X coordinate on the map
+    int yPos_;      // Y coordinate on the map
     int hp_;        // health points
     int pwr_;       // power
     int amr_;       // armor
